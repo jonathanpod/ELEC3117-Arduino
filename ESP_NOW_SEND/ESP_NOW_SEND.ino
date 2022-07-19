12a9a5a3527232db2a68a016d3bb7f08f83f643f
Jonathan Podiono <jonathanpodiono@yahoo.com>
2022-07-19 16:43:11 +1000

    ESP_NOW_SEND: Completed code for sending data at an interval, averaging, and during abnormalities.

--- ESP_NOW_SEND/ESP_NOW_SEND.ino
+++ ESP_NOW_SEND/ESP_NOW_SEND.ino
@@ -1,11 +1,3 @@
-/*
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*/
-
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 #include <Wire.h>
@@ -14,11 +6,11 @@
 // Initialise the Adafruit temperature sensor.
 Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
 
-// Define the constants
+// ----------------- CONSTANTS -------------------
 #define HIGH_TEMP 28
 #define LOW_TEMP 20
 
-// REPLACE WITH RECEIVER MAC Address
+// ----------------- WI-FI INITIALISATION -----------------
 uint8_t broadcastAddress[] = {0xA8, 0x48, 0xFA, 0xE6, 0x58, 0x55};
 
 // Structure example to send data
@@ -30,9 +22,13 @@ typedef struct struct_message {
 // Create a struct_message called myData
 struct_message myData;
 
+// ----------------- GLOBAL VARIABLES -------------------
 unsigned long last_time = 0;  
 unsigned long timer_delay = 360000; // Timer delay = 6 minutes initially
-unsigned long timer_delay_30_minutes = 1800000;
+int reading_count = 0;
+float temperature_array[5];
+
+// --------------------- PRE-DEFINED FUNCTIONS --------------------------
 
 // Callback when data is sent
 void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
@@ -44,7 +40,8 @@ void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
     Serial.println("Delivery fail");
   }
 }
- 
+
+// ---------------------- MAIN -----------------------
 void setup() {
   // Init Serial Monitor
   Serial.begin(9600);
@@ -74,15 +71,11 @@ void setup() {
 
   // Temperature Sensor Setup
   tempsensor.setResolution(3);
-
-  // Initialise temperature array
-  uint8_t temperature_array[5];
-  int reading_count = 0;
 }
 
 
 void loop() {
-  if ((millis() - lastTime) > timer_delay) {    
+  if ((millis() - last_time) > timer_delay) {    
     
     // Wake up sensor and read temperature.
     tempsensor.wake(); 
@@ -92,45 +85,40 @@ void loop() {
     tempsensor.shutdown_wake(1); // shutdown MSP9808 - power consumption ~0.1 mikro Ampere, stops temperature sampling
     delay(200);
 
-    // Append to temperature_array.
-    temperature_array[reading_count];
-    
     // Set values to send
     myData.temp_c = c;
     Serial.print(c, 4); 
     Serial.println("*C\t");
     
-    reading_count++;  // Increment the reading counter.
-
     // If reading is higher or lower than threshold.
     if (c >= HIGH_TEMP || c <= LOW_TEMP) {
       // Increase sampling time and alert the receiver.
       esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));  // Send data to RX
       timer_delay = 10000;  // Set new sampling interval to 10 seconds.
-    } else {
-      timer_delay = 360000; // Else set it to 6 minutes.        
-    }
 
-
-
-
-    
-    // Send message via ESP-NOW
-    //esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
-
-    // If it has read 5 values, average and reset the array.
-    if (reading_count >= 5) {
-      float avg_temp = temperature_array[0] + temperature_array[1] + temperature_array[2] + temperature_array[3] + temperature_array[4];
-      avg_temp = (1/5) * avg_temp;
-
-      // Set values to send
-      myData.temp_c = c;
-      esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));  // Send data to RX
-      
       reading_count = 0;
-    }
+      
+    } else {
+      timer_delay = 360000; // Else set it to 6 minutes.
+      
+      // Append to temperature_array.
+      temperature_array[reading_count];    
+      reading_count++;
 
+      // If it has read 5 values, average and reset the counter.
+      // Normal state means that temperature value is within the range.
+      if (reading_count >= 5) {
+        float avg_temp = temperature_array[0] + temperature_array[1] + temperature_array[2] + temperature_array[3] + temperature_array[4];
+        avg_temp = (1/5) * avg_temp;
+
+        // Set values to send
+        myData.temp_c = avg_temp;
+        esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));  // Send data to RX
+      
+        reading_count = 0;
+      }
+    }
+    
     last_time = millis();  // Update the last_time variable
   }
-  
 }
