ebf4eb0fa24711f44ba972ca7126d69ef3d09555
Jonathan Podiono <jonathanpodiono@yahoo.com>
2022-07-24 20:00:46 +1000

    RX_MAIN: Added code to display temperature.

--- RX_MAIN/RX_MAIN.ino
+++ RX_MAIN/RX_MAIN.ino
@@ -1,5 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 // ----------------- WI-FI INITIALISATION -----------------
 // Structure example to receive data
@@ -8,6 +11,11 @@ typedef struct struct_message {
     float temp_c;
 } struct_message;
 
+// ----------------- OLED INITIALISATION ------------------
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+//
 // Create a struct_message called myData
 struct_message myData;
 
@@ -18,13 +26,31 @@ struct_message myData;
 // --------------------- USER-DEFINED FUNCTIONS --------------------------
 
 // Callback function that will be executed when data is received
-
 void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
   memcpy(&myData, incomingData, sizeof(myData));
   Serial.print("Temperature: ");
   Serial.print(myData.temp_c); 
   Serial.print("*C");
   Serial.println();
+}
+
+// Display the temperature in the OLED display.
+void display_temperature(float temp_c){
+  
+  // Clear the display
+  display.clearDisplay();
+  //Set the color - always use white despite actual display color
+  display.setTextColor(WHITE);
+  //Set the font size
+  display.setTextSize(1);
+  //Set the cursor coordinates
+  display.setCursor(0,0);
+  display.print("EnviroHub");
+  display.setCursor(0,10); 
+  display.print("Temperature:    "); 
+  display.print(temp_c);
+  display.print(" C");
+
 }
 
 // ----------------- GLOBAL VARIABLES -------------------
@@ -35,6 +61,12 @@ void setup() {
   // Initialize Serial Monitor
   Serial.begin(9600);
 
+  // Begin Wire library for I2C
+  Wire.begin();
+  
+  // Initialize OLED with I2C address 0x3D
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3D);
+  
   // Initialise pins
   pinMode(15, OUTPUT);  // Buzzer Output
   pinMode(13, INPUT);   // Button Input
@@ -43,7 +75,7 @@ void setup() {
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
-  // Init ESP-NOW
+  // Initialise ESP-NOW
   if (esp_now_init() != 0) {
     Serial.println("Error initializing ESP-NOW");
     return;
@@ -54,9 +86,8 @@ void setup() {
   esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
   esp_now_register_recv_cb(OnDataRecv);
 
-  // Initialise temp_c
+  // Initialise temp_c to room temperature.
   myData.temp_c = 24;
-  
 }
 
 void loop() {
@@ -84,5 +115,7 @@ void loop() {
     digitalWrite(16, LOW);
   }
 
-  
+  // --------- Display in OLED -----------
+  display_temperature(myData.temp_c);
+  display.display();    // Show the display buffer on the screen 
 }
